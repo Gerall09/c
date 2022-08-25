db521bab445beb7dab26712130cb2951b0e55bd7
Gerall09 <103030918+Gerall09@users.noreply.github.com>
2022-08-25 21:33:56 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['6287894034440','6282196995322','6285891214734','62895384910282'] //owner
+global.wame = ['6282196995322'] 
+global.pemilik = ['6282196995322'] //owner
+global.premium = ['6287894034440'] //premium
+global.pengguna = 'Gerall' //username
+global.footer = 'WhanSaff'
+global.botnma = 'WhanSaff' //bot name
+global.botname = 'Todz'
+global.ownernma = 'Gerall' //owner name
+global.packname = 'Gerall' //sticker package name
+global.author = 'By Gerall Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
@@ -55,7 +55,7 @@ global.available = false
 global.unavailable = true
 global.limittot = 100,
 global.thumb = fs.readFileSync('./Zimbot/drips.jpg')
-global.reactmoji = '🇿🇼'
+global.reactmoji = '🇮🇩'
 global.imgjoinrpg = fs.readFileSync('./image/joinrpg.jpg')
 global.imgmining = fs.readFileSync('./image/mining.jpg')
 global.imgdevil = fs.readFileSync('./image/img/devil.jpg')
